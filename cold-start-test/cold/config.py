b422c5436195879f41c4cb9e1320d3d0ee16c6af
yskim <yskim@huinno.com>
2022-02-18 10:23:07 +0900

    ORM 및 database 연결

---
+++ cold-start-test/cold/config.py
@@ -0,0 +1,12 @@
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
